cce5c9d9fbf437668a11297d71ebc656f9e9d503
UsenkoEvgeniy <usenko.evgeniy@gmail.com@users.noreply.github.com>
2020-10-03 10:33:51 +0300

    HW 01 refactor with jstl

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -5,15 +5,13 @@ import ru.javawebinar.topjava.model.MealTo;
 import ru.javawebinar.topjava.model.Meals;
 import ru.javawebinar.topjava.util.MealsUtil;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
@@ -24,24 +22,9 @@ public class MealServlet  extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         log.debug("redirect to meals");
-
-        resp.setCharacterEncoding("UTF-8");
-        resp.setContentType("text/html");
-        PrintWriter out=resp.getWriter();
-        out.println("<a href='index.html'>Home</a>");
-        out.println("<h1>Meals</h1>");
-
         List<MealTo> mealList = MealsUtil.filteredByStreams(Meals.getData(), LocalTime.MIN, LocalTime.MAX, 2000);
-
-        out.print("<table border='1'");
-        out.print("<tr><th>Date</th><th>Description</th><th>Calories</th></tr>");
-        DateTimeFormatter tableDate = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-        for(MealTo meal :mealList){
-            out.print("<tr style=\"color:" + (meal.isExcess()?"red":"green") + "\"><td>"+tableDate.format(meal.getDateTime())
-                    + "</td><td>"+meal.getDescription() + "</td><td>" + meal.getCalories() + "</td></tr>");
-        }
-        out.print("</table>");
-
-        out.close();
+        req.setAttribute("mealList", mealList);
+        RequestDispatcher rd = getServletContext().getRequestDispatcher("/meals.jsp");
+        rd.forward(req, resp);
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -7,6 +7,8 @@
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html lang ="ru">
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page import="java.time.format.DateTimeFormatter" %>
 <head>
     <title>Meals</title>
 </head>
@@ -14,6 +16,23 @@
 <h3><a href="index.html">Home</a> </h3>
 <hr>
 <h2>Meals</h2>
-
+<table border="1" cellspacing="0">
+    <thead>
+    <tr>
+        <th>Date</th>
+        <th>Description</th>
+        <th>Calories</th>
+    </tr>
+    </thead>
+    <tbody>
+        <c:forEach items = "${mealList}" var="meal">
+            <tr style="color:${meal.excess? ' green':' red'}">
+                <td><c:out value="${meal.dateTime.format(DateTimeFormatter.ofPattern('dd.MM.yyyy HH:mm'))}" /></td>
+                <td><c:out value="${meal.description}" /></td>
+                <td><c:out value="${meal.calories}" /></td>
+            </tr>
+        </c:forEach>
+    </tbody>
+</table>
 </body>
 </html>
